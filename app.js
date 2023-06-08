550830e41f47258d194b4a953ab971758ee25fea
Praise Ebuka <praise.njoga@gmail.com>
2023-06-08 18:40:30 +0100

    Change is inevitable.

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000);
 
-console.log("Server is running on port 3000...");
+
+//console.log("Server is running on port 3000...");
 
 
